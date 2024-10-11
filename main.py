32fd740232d04e0d7dc9fa1f1539b74b66ae5daf
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2024-10-11 06:12:13 +0300

    Generate ER diagram

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from app import create_app
+from app.models import Product, Order, OrderItem, User
+from sqlalchemy_data_model_visualizer import generate_data_model_diagram
 
 # Create the app instance
 app = create_app()
+
+# Generate ER Diagram
+generate_data_model_diagram([Product, Order, OrderItem, User], "diagram")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
