1e762382ec63d20aa2b65336dc4ab4a4b10daee6
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2024-10-11 03:17:25 +0300

    Initialize flask application with extensions (sqlalchemy, jwt)

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+
+# Initialize extensions
+db = SQLAlchemy()
+jwt = JWTManager()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.config['SECRET_KEY'] = 'SECRET'
+    # Database settings
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # JWT configuration
+    app.config['JWT_SECRET_KEY'] = 'JWT_KEY'
+
+    # Initialize extensions with app instance
+    db.init_app(app)
+    jwt.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
+
---
+++ run.py
@@ -0,0 +1,8 @@
+from app import create_app
+
+# Create the app instance
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
