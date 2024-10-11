be0bc150197429d6bb1f60fe618c9aed7f83be11
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2024-10-11 03:29:03 +0300

    Add `/register` endpoint

---
+++ app/routes.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, request, jsonify
+from app.models import User
+from app import db
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.exc import IntegrityError
+
+main_blueprint = Blueprint('main', __name__)
+
+@main_blueprint.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    
+    if not all([data.get('first_name'), data.get('last_name'), data.get('email'),
+                data.get('phone_number'), data.get('password')]) or data.get('is_admin') is None:
+        return jsonify({'error': 'All fields are required'}), 400
+
+    hashed_password = generate_password_hash(data['password'])
+
+    new_user = User(
+        first_name=data['first_name'],
+        last_name=data['last_name'],
+        email=data['email'],
+        phone_number=data['phone_number'],
+        password_hash=hashed_password,
+        is_admin = data['is_admin']
+    )
+
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'message': 'User registered successfully'}), 201
+    except IntegrityError:
+        return jsonify({'error': 'User with this email already exists'}), 400
+
